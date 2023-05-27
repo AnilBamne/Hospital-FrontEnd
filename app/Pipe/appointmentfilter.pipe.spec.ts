ce1582ae6ae92afe104b21a486357bcc8e1808b3
Anil Bamne <99521630+AnilBamne@users.noreply.github.com>
2023-05-27 23:26:26 +0530

    Delete app directory

--- app/Pipe/appointmentfilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppointmentfilterPipe } from './appointmentfilter.pipe';
-
-describe('AppointmentfilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AppointmentfilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- app/Pipe/patientfilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PatientfilterPipe } from './patientfilter.pipe';
-
-describe('PatientfilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PatientfilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
